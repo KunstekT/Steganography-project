5e617cadd73fe2b7169306212cacf3df83c20ce9
Toni <tony.kunstek@gmail.com>
2022-02-27 23:41:30 +0100

    Added/removed some images

--- Steganography-project/Steganography_project.py
+++ Steganography-project/Steganography_project.py
@@ -25,6 +25,8 @@ class SteganographyEncoder:
     encodedImage = np.zeros([100,100,3],dtype=np.uint8)
     encodedImage.fill(0) # or img[:] = 255
 
+    messageToEncode =""
+
     def loadImage(self, link):
         #self.image = mpimg.imread(link)
         self.image = cv.imread(link, cv.IMREAD_COLOR)
@@ -65,19 +67,35 @@ class SteganographyEncoder:
     def printByte(self, a):
         print(format(a, '0{}b'.format(8)))
 
-    def encodeMessage(self, message):
-        showEncodedMessage = 1
-
-        binaryMessageStringList = self.messageToBinary(message)
+    def GetBinaryMessageString(self):
+        binaryMessageStringList = self.messageToBinary(self.messageToEncode)
         binaryMessageString=""
         for s in binaryMessageStringList:
             binaryMessageString+=s
+        return binaryMessageString
+
+    def encodeMessage(self, message):
+        self.messageToEncode = message
+        showEncodedMessage = 1
+        startWith = 0
+
+        #binaryMessageStringList = self.messageToBinary(message)
+        #binaryMessageString=""
+        #for s in binaryMessageStringList:
+        #    binaryMessageString+=s
+        binaryMessageString = self.GetBinaryMessageString()
 
         #print("Binarni niz: ", binaryMessageString)
         #print("Duljina binarnog niza: ", len(binaryMessageString))
         
         counter = -1
         for color in range(3):
+            if(startWith >= 2):
+                startWith = 1
+                continue
+            if(startWith == 1):
+                startWith = 0
+                continue
             for i in range(np.size(self.encodedImage, 0)):
                 for j in range(np.size(self.encodedImage, 1)):
                     counter+=1
@@ -109,6 +127,10 @@ class SteganographyEncoder:
                             self.encodedImage[i,j,color] = a
         if(counter < len(binaryMessageString)):
             print("Warning: Message is not fully encoded.")
+
+    def GetMessageSize(self):
+
+        return len(messageToEncode)
         
 class SteganographyDecoder:
 
@@ -221,6 +243,9 @@ class Steganalyzer:
         plt.xlabel("Color value")
         plt.ylabel("Pixels")
         plt.title("Histogram comparision")
+        plt.legend(['lenna.png', 'beach_indexed.png'])
+        #plt.legend(['Unedited image', 'Edited image'])
+
   
         # ? Prints out comparison of values needed for chi test
     def printComparisionTable(self, OddIndexAverages, e, OddIndexAverages2, e2):
@@ -331,7 +356,7 @@ class Steganalyzer:
         if(nRowsToCheck>self.image.shape[1]):
             nRowsToCheck = self.image.shape[1]
 
-        print("nRowsToCheck: ", nRowsToCheck, "[", nRowsToCheck/self.image.shape[1]*100, "%]")
+        print("Number of image rows to analyze: ", nRowsToCheck, "[", nRowsToCheck/self.image.shape[1]*100, "%]")
 
         image2 = np.zeros([100,100,3],dtype=np.uint8)
         image2 = self.image[0:nRowsToCheck,:,:]
@@ -352,42 +377,66 @@ class Steganalyzer:
         #self.PrintHistogramValues(histogram_B, e2, OddIndexes2)
         result2 = self.ChiSquareTest(OddIndexes2, e2)
 
-        print("Probability of data in red channel: ", result0)
-        print("Probability of data in green channel: ", result1)
-        print("Probability of data in blue channel: ", result2)        
+        print("Probability of hidden message in the red channel: {:.2f}%".format(result0*100))
+        print("Probability of hidden message in the green channel: {:.2f}%".format(result1*100))
+        print("Probability of hidden message in the blue channel: {:.2f}%".format(result2*100))        
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n") 
 
+
 #messageToEncode = ""
-#messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-#messageToEncode3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis ante sit amet erat porttitor luctus. Sed justo nulla, interdum ac elit eget, viverra elementum est. Quisque porta metus nisi, nec malesuada dolor suscipit id. Ut vehicula congue nulla, vitae mattis tortor sollicitudin pellentesque."
-#messageToEncode2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lobortis neque, sed varius lacus. Etiam dapibus iaculis efficitur. Duis lectus nisl, tincidunt nec placerat sed, porta ac elit. Maecenas leo lacus, congue ac quam mattis, luctus vestibulum sem. Quisque eget nisi eu mi tempus dictum. Aenean eu suscipit felis, vitae porttitor mauris. Maecenas velit nunc, vulputate nec tristique vel, consequat in ipsum. Ut sed metus eget tellus bibendum laoreet quis ut sem. Vestibulum quis vestibulum sem. Curabitur condimentum augue vel eros sagittis ultricies. Vivamus ut est commodo, lacinia lorem non, mattis nunc. Curabitur mattis tortor lobortis vehicula ultricies. Aenean tortor lorem, venenatis nec sollicitudin non, scelerisque ac erat. Mauris iaculis eleifend neque. Ut vitae laoreet eros. Donec fringilla enim eu arcu aliquet fermentum. Donec malesuada sollicitudin nisl non faucibus. Sed eleifend vulputate vehicula. Ut et lorem tempor, porta arcu vitae, egestas nunc. Integer tempus vitae nulla vitae blandit."
-#messageToEncode2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lobortis neque, sed varius lacus. Etiam dapibus iaculis efficitur. Duis lectus nisl, tincidunt nec placerat sed, porta ac elit. Maecenas leo lacus, congue ac quam mattis, luctus vestibulum sem. Quisque eget nisi eu mi tempus dictum. Aenean eu suscipit felis, vitae porttitor mauris. Maecenas velit nunc, vulputate nec tristique vel, consequat in ipsum. Ut sed metus eget tellus bibendum laoreet quis ut sem. Vestibulum quis vestibulum sem. Curabitur condimentum augue vel eros sagittis ultricies. Vivamus ut est commodo, lacinia lorem non, mattis nunc. Curabitur mattis tortor lobortis vehicula ultricies. Aenean tortor lorem, venenatis nec sollicitudin non, scelerisque ac erat. Mauris iaculis eleifend neque. Ut vitae laoreet eros. Donec fringilla enim eu arcu aliquet fermentum. Donec malesuada sollicitudin nisl non faucibus. Sed eleifend vulputate vehicula. Ut et lorem tempor, porta arcu vitae, egestas nunc. Integer tempus vitae nulla vitae blandit. Nam in dolor ac metus sodales tincidunt. Integer venenatis urna ligula, et mattis mauris aliquam quis. Donec sit amet nulla vel orci faucibus luctus. Pellentesque commodo nisi dolor, ut egestas elit venenatis ac. Aenean in felis viverra, interdum magna id, consectetur magna. Nullam mollis tincidunt nisi, sed blandit dui bibendum vitae. In suscipit, nisi sed gravida malesuada, nibh libero maximus dolor, non eleifend nisl elit a nisi. Praesent ac luctus tellus. Quisque imperdiet purus eget maximus feugiat. Curabitur sollicitudin rhoncus finibus. Aliquam vehicula tincidunt enim, in rutrum ipsum cursus ac. Nulla gravida dui eget augue fringilla, mollis gravida metus euismod."
-messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lobortis neque, sed varius lacus. Etiam dapibus iaculis efficitur. Duis lectus nisl, tincidunt nec placerat sed, porta ac elit. Maecenas leo lacus, congue ac quam mattis, luctus vestibulum sem. Quisque eget nisi eu mi tempus dictum. Aenean eu suscipit felis, vitae porttitor mauris. Maecenas velit nunc, vulputate nec tristique vel, consequat in ipsum. Ut sed metus eget tellus bibendum laoreet quis ut sem. Vestibulum quis vestibulum sem. Curabitur condimentum augue vel eros sagittis ultricies. Vivamus ut est commodo, lacinia lorem non, mattis nunc. Curabitur mattis tortor lobortis vehicula ultricies. Aenean tortor lorem, venenatis nec sollicitudin non, scelerisque ac erat. Mauris iaculis eleifend neque. Ut vitae laoreet eros. Donec fringilla enim eu arcu aliquet fermentum. Donec malesuada sollicitudin nisl non faucibus. Sed eleifend vulputate vehicula. Ut et lorem tempor, porta arcu vitae, egestas nunc. Integer tempus vitae nulla vitae blandit. Nam in dolor ac metus sodales tincidunt. Integer venenatis urna ligula, et mattis mauris aliquam quis. Donec sit amet nulla vel orci faucibus luctus. Pellentesque commodo nisi dolor, ut egestas elit venenatis ac. Aenean in felis viverra, interdum magna id, consectetur magna. Nullam mollis tincidunt nisi, sed blandit dui bibendum vitae. In suscipit, nisi sed gravida malesuada, nibh libero maximus dolor, non eleifend nisl elit a nisi. Praesent ac luctus tellus. Quisque imperdiet purus eget maximus feugiat. Curabitur sollicitudin rhoncus finibus. Aliquam vehicula tincidunt enim, in rutrum ipsum cursus ac. Nulla gravida dui eget augue fringilla, mollis gravida metus euismod. Donec vitae sapien non arcu viverra euismod. Nam dapibus ultricies leo sed mollis. Donec cursus risus arcu, sit amet faucibus leo pulvinar vitae. Nam commodo volutpat porttitor. Ut convallis hendrerit sapien, in laoreet justo rutrum sed. Duis a ligula et diam consectetur lacinia sagittis quis ipsum. Integer accumsan dolor urna, sed suscipit risus pharetra sed. Nullam cursus libero sed elit aliquet aliquet. Praesent non elementum ligula. Aenean in libero at odio tristique vulputate nec sed augue. Ut tellus enim, consequat eget hendrerit non, sagittis vel turpis. Aliquam ullamcorper rhoncus fermentum. In commodo sodales nibh et rhoncus. Maecenas at diam vestibulum arcu molestie rhoncus quis sed nunc. Aliquam blandit faucibus erat, et pulvinar neque suscipit at. Pellentesque erat elit, pellentesque id pharetra sed, laoreet non risus. Donec auctor euismod fringilla. Curabitur dapibus elit sollicitudin eros finibus feugiat et vehicula lectus. Nunc feugiat risus et tristique bibendum. Etiam porttitor, massa sed laoreet porttitor, nibh arcu vehicula felis, faucibus semper dolor augue non felis. Sed et diam fringilla, viverra ante vel, interdum tortor. Cras semper ipsum non purus tincidunt accumsan. Integer dui magna, feugiat at rutrum a, rhoncus sed nunc. In hac habitasse platea dictumst."
-messageToEncode = messageToEncode + messageToEncode
-messageToEncode = messageToEncode + messageToEncode #+ messageToEncode
-messageToEncode = messageToEncode + messageToEncode
-messageToEncode = messageToEncode + messageToEncode
+#messageToEncode = "Lorem"
+#messageToEncode = "Lorem ipsum"
+messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+#messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis ante sit amet erat porttitor luctus. Sed justo nulla, interdum ac elit eget, viverra elementum est. Quisque porta metus nisi, nec malesuada dolor suscipit id. Ut vehicula congue nulla, vitae mattis tortor sollicitudin pellentesque."
+#messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non lobortis neque, sed varius lacus. Etiam dapibus iaculis efficitur. Duis lectus nisl, tincidunt nec placerat sed, porta ac elit. Maecenas leo lacus, congue ac quam mattis, luctus vestibulum sem. Quisque eget nisi eu mi tempus dictum. Aenean eu suscipit felis, vitae porttitor mauris. Maecenas velit nunc, vulputate nec tristique vel, consequat in ipsum. Ut sed metus eget tellus bibendum laoreet quis ut sem. Vestibulum quis vestibulum sem. Curabitur condimentum augue vel eros sagittis ultricies. Vivamus ut est commodo, lacinia lorem non, mattis nunc. Curabitur mattis tortor lobortis vehicula ultricies. Aenean tortor lorem, venenatis nec sollicitudin non, scelerisque ac erat. Mauris iaculis eleifend neque. Ut vitae laoreet eros. Donec fringilla enim eu arcu aliquet fermentum. Donec malesuada sollicitudin nisl non faucibus. Sed eleifend vulputate vehicula. Ut et lorem tempor, porta arcu vitae, egestas nunc. Integer tempus vitae nulla vitae blandit. Nam in dolor ac metus sodales tincidunt. Integer venenatis urna ligula, et mattis mauris aliquam quis. Donec sit amet nulla vel orci faucibus luctus. Pellentesque commodo nisi dolor, ut egestas elit venenatis ac. Aenean in felis viverra, interdum magna id, consectetur magna. Nullam mollis tincidunt nisi, sed blandit dui bibendum vitae. In suscipit, nisi sed gravida malesuada, nibh libero maximus dolor, non eleifend nisl elit a nisi. Praesent ac luctus tellus. Quisque imperdiet purus eget maximus feugiat. Curabitur sollicitudin rhoncus finibus. Aliquam vehicula tincidunt enim, in rutrum ipsum cursus ac. Nulla gravida dui eget augue fringilla, mollis gravida metus euismod."
+#messageToEncode = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nunc quam, scelerisque sed ex sit amet, laoreet tincidunt lacus. Vivamus aliquet elit vitae pretium semper. Sed faucibus quam maximus sapien convallis volutpat. Cras consectetur mi vel lacus elementum efficitur. Cras pharetra nibh sit amet neque varius sollicitudin. Nulla aliquet eros a mi tincidunt tempor. Etiam quis nisi iaculis, venenatis tellus ac, fringilla urna. Duis condimentum dui et diam vulputate tincidunt. Nulla quis nulla ut elit bibendum suscipit. Nam nisi sem, sodales at elementum eget, consequat in tellus. Nam ante urna, tincidunt id malesuada ut, porttitor ac purus. Cras consequat ipsum sed enim euismod, vitae tempor mauris aliquet. Curabitur commodo malesuada ornare. Curabitur eget nibh tellus. Curabitur vitae nulla lacinia urna pharetra blandit at non tortor. Fusce a tellus eu ante euismod convallis in eget nulla. Donec tortor nulla, cursus cursus sagittis non, semper sed neque. Ut pretium ante id imperdiet consectetur. Pellentesque tempus turpis sit amet risus fermentum maximus. Proin id hendrerit nulla. Praesent finibus, libero id efficitur ultricies, velit diam dapibus purus, a suscipit mi urna a lorem. Aenean vulputate facilisis arcu, a volutpat mauris rhoncus eget. Aliquam quis mollis risus, ut consequat elit. Morbi aliquet velit sagittis aliquet porttitor. Donec vel arcu ut elit lacinia varius. Aliquam justo est, finibus non erat a, lacinia feugiat ligula. Nulla lacinia faucibus enim sed pharetra. Maecenas quam leo, convallis sit amet lectus at, cursus ornare sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean ut mauris semper, porta tellus a, ultrices urna. In ac risus dolor. Donec interdum rhoncus velit nec elementum. Sed ut congue augue. Aliquam erat volutpat. Pellentesque at velit sit amet est iaculis pharetra. Aliquam pellentesque vestibulum varius. Vivamus commodo diam leo, quis eleifend eros bibendum et. Nam semper nibh eu posuere faucibus. Curabitur condimentum vitae nunc non faucibus. Nullam accumsan et nulla a eleifend. Ut ligula est, pellentesque quis est vel, dictum molestie urna. Aenean id elit in ante fermentum ornare. Cras accumsan leo eget velit blandit, vel aliquam nisl tincidunt. Morbi venenatis velit in neque iaculis, at posuere arcu dapibus. Aenean pellentesque ullamcorper eros vel elementum. Nullam justo quam, hendrerit ut vehicula vel, malesuada scelerisque lectus. In sed purus at tellus rutrum hendrerit. In eget vehicula dui. Sed in ante et turpis interdum ultrices. In ex velit, cursus non est quis, aliquam ultricies massa. Sed fringilla sed libero dapibus pharetra. Fusce in orci vitae augue ultrices auctor malesuada a quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent pulvinar sed velit non hendrerit. Pellentesque aliquet sagittis sem, ut pharetra sem tempus id. Proin ut quam pharetra, malesuada ante eu, interdum nisl. Aliquam lobortis in odio ac efficitur. Cras rutrum leo non enim pretium, eget porttitor libero tincidunt. Mauris arcu lectus, semper sit amet justo eget, tempor egestas arcu."
+#messageToEncode = messageToEncode + messageToEncode
+#messageToEncode = messageToEncode + messageToEncode #+ messageToEncode
+#messageToEncode = messageToEncode + messageToEncode
+#messageToEncode = messageToEncode + messageToEncode
 
 encoder = SteganographyEncoder()
-encoder.loadImage("images/lenna.png")
+encoder.loadImage("images/beach_indexed.png")
 encoder.encodeMessage(messageToEncode)
 encoder.saveImage("images/saved.png")
+#print("Message size: ", encoder.GetMessageSize())
+#print("Message size (binary):", len(encoder.GetBinaryMessageString()))
 
 decoder = SteganographyDecoder()
 decoder.loadImage("images/saved.png")
 decoder.decode(1)
 
-print("\n")
-print("********************** Edited image steganalysis: **********************")
+
+#print("\n")
+#print("********************** Edited image steganalysis: **********************")
 steganalyzer = Steganalyzer()
 steganalyzer.LoadImage("images/saved.png")
-steganalyzer.Analyze(100)
+steganalyzer.Analyze(32)
 
-print("\n")
-print("************************************************************************")
-print("********************* Unedited image steganalysis: *********************")
+#print("\n")
+#print("********************** Edited image steganalysis: **********************")
+steganalyzer = Steganalyzer()
+steganalyzer.LoadImage("images/saved.png")
+steganalyzer.Analyze(2)
+
+#print("\n")
+#print("********************** Edited image steganalysis: **********************")
+steganalyzer = Steganalyzer()
+steganalyzer.LoadImage("images/saved.png")
+steganalyzer.Analyze(1)
+
 steganalyzer.LoadImage("images/lenna.png")
-steganalyzer.Analyze(100)
+h1, b1 = steganalyzer.getHistogram(steganalyzer.image,0)
+steganalyzer.LoadImage("images/saved.png")
+h2, b2 = steganalyzer.getHistogram(steganalyzer.image,0)
+steganalyzer.compareHistograms(h1,h2,b1,b2,"red","blue")
+plt.show()
+
+#print("\n")
+#print("************************************************************************")
+#print("********************* Unedited image steganalysis: *********************")
+#steganalyzer.LoadImage("images/lenna.png")
+#steganalyzer.Analyze(512)
 
 #plt.figure()
 #plt.imshow(cv.cvtColor(encoder.image, cv.COLOR_BGR2RGB))
